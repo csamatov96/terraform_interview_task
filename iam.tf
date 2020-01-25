6cf0d3cdb24482fce1949f44f1d4eb65e7a7941c
csamatov96 <csamatov96@gmail.com>
2020-01-25 12:54:00 -0600

    changes

--- iam.tf
+++ iam.tf
@@ -1,5 +1,5 @@
-resource “aws_iam_role” “test_role” { 
-name = “test_role” 
+resource "aws_iam_role" "test_role" { 
+name = "test_role" 
 assume_role_policy = <<EOF
 { 
   “Version”: “2012-10-17”, 
@@ -15,4 +15,4 @@ assume_role_policy = <<EOF
   ] 
 }
 EOF
-}
+} 
