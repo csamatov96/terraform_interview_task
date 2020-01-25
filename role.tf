754a8bb848d989b3bc9c347535ed6cb6dda24866
csamatov96 <csamatov96@gmail.com>
2020-01-25 12:37:40 -0600

    added

--- role.tf
+++ role.tf
@@ -1,22 +1,5 @@
-resource "aws_iam_role" "s3role" { 
-name = "s3role" 
-assume_role_policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-        "Sid": "ListObjectsInBucket",
-        "Effect": "Allow",
-        "Action": ["s3:ListBucket"],
-        "Resource": ["arn:aws:s3:::buckewtdf"]
-        },
-        {
-        "Sid": "AllObjectActions",
-        "Effect": "Allow",
-        "Action": "s3:*Object",
-        "Resource": ["arn:aws:s3:::buckewtdf/*"]
-        }
-    ]
+resource "aws_iam_policy_attachment" "test-attach" {
+  name       = "test-attachment"
+  roles      = ["${aws_iam_role.s3_access_role.name}"]
+  policy_arn = "${aws_iam_policy.policy.arn}"
 }
-EOF
-} 
