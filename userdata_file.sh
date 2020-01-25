6c7c6b55f61b9cd4c4fc6ebfb17a97df7f8d1d7b
csamatov96 <csamatov96@gmail.com>
2020-01-25 13:27:03 -0600

    changes

--- userdata_file.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-"sudo yum update -y",
-"sudo yum install httpd -y",
-"sudo service httpd start",
-"sudo chkconfig httpd on",
-"echo “HELLO WORLD” > /var/www/html/index.html",
-"echo “HELLO WORLD from $(hostname -f)” > /var/www/html/index.html"
