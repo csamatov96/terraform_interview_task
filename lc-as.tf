9fa7a0a2bb3dd6ae9f7d5c576522d7f17e213177
csamatov96 <csamatov96@gmail.com>
2020-01-25 12:23:43 -0600

    changes

--- lc-as.tf
+++ lc-as.tf
@@ -2,7 +2,7 @@ resource "aws_launch_template" "example" {
   name_prefix   = "example"
   image_id      = "${data.aws_ami.centos.id}"
   instance_type = "t2.micro"
-  security_groups = ["allow_http_https"]
+  #security_groups = ["allow_http_https"]
   user_data = file("userdata_file")
 }
 
