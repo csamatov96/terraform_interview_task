8374cb6e11365c27831e22d3fd53f905aeecbcea
csamatov96 <csamatov96@gmail.com>
2020-01-25 13:32:00 -0600

    name edited

--- main.tf
+++ main.tf
@@ -11,6 +11,6 @@ resource "aws_iam_policy" "policy" {
 
 resource "aws_iam_policy_attachment" "test-attach" {
   name       = "test-attachment"
-  roles      = ["${aws_iam_role.test.name}"]
+  roles      = ["${aws_iam_role.custom-role.name}"]
   policy_arn = "${aws_iam_policy.policy.arn}"
 }
