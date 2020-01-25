ce803da3ce143ca6fc924e88484c7d5b34b20213
csamatov96 <csamatov96@gmail.com>
2020-01-25 11:37:57 -0600

    sent

---
+++ ec2.tf
@@ -0,0 +1,23 @@
+data "aws_ami" "CentOS Linux 6" { #
+    filter { 
+        name = "root-device-type" 
+        values = ["ebs"] #instance_store  
+        } 
+    most_recent = true 
+    owners = ["679593333241"] 
+} 
+
+#show the AMI id 
+output "ami" { 
+    value = "${data.aws_ami.centos.id}" 
+} 
+
+resource "aws_instance" "web" {
+    ami           = "${data.aws_ami.centos.id}"
+    instance_type = "t2.micro"
+
+    tags = {
+        Name = "HelloWorld"
+    } 
+}
+
