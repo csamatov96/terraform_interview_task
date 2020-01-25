2e84e1ac6ac897df9111759a8666495b5fe11b11
csamatov96 <csamatov96@gmail.com>
2020-01-25 12:26:28 -0600

    changes

--- role.tf
+++ role.tf
@@ -5,16 +5,16 @@ assume_role_policy = <<EOF
     "Version": "2012-10-17",
     "Statement": [
         {
-            "Sid": "ListObjectsInBucket",
-            "Effect": "Allow",
-            "Action": ["s3:ListBucket"],
-            "Resource": ["arn:aws:s3:::buckewtdf"]
+        "Sid": "ListObjectsInBucket",
+        "Effect": "Allow",
+        "Action": ["s3:ListBucket"],
+        "Resource": ["arn:aws:s3:::buckewtdf"]
         },
         {
-            "Sid": "AllObjectActions",
-            "Effect": "Allow",
-            "Action": "s3:*Object",
-            "Resource": ["arn:aws:s3:::buckewtdf/*"]
+        "Sid": "AllObjectActions",
+        "Effect": "Allow",
+        "Action": "s3:*Object",
+        "Resource": ["arn:aws:s3:::buckewtdf/*"]
         }
     ]
 }
