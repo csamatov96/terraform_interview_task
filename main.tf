bfb1103a9ac78b419d8536462b4348c7daf5cefb
csamatov96 <csamatov96@gmail.com>
2020-01-25 13:16:32 -0600

    added

---
+++ main.tf
@@ -0,0 +1,16 @@
+resource "aws_iam_role" "test" {
+  name               = "test-role"
+  assume_role_policy = "${file("assume-role-policy.json")}"
+}
+
+resource "aws_iam_policy" "policy" {
+  name        = "test-policy"
+  description = "A test policy"
+  policy      = "${file("policy-s3-bucket.json")}"
+}
+
+resource "aws_iam_policy_attachment" "test-attach" {
+  name       = "test-attachment"
+  roles      = ["${aws_iam_role.test.name}"]
+  policy_arn = "${aws_iam_policy.policy.arn}"
+}
