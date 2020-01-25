12555d24b145b4c3f8dcb9a133a0bd6e0b00db60
csamatov96 <csamatov96@gmail.com>
2020-01-25 11:50:55 -0600

    added

---
+++ lc-as.tf
@@ -0,0 +1,21 @@
+resource "aws_launch_template" "example" {
+  name_prefix   = "example"
+  image_id      = "${data.aws_ami.centos.id}"
+  instance_type = "t2.micro"
+}
+
+resource "aws_autoscaling_group" "example" {
+  availability_zones = ["eu-west-3a"]
+  desired_capacity   = 3
+  max_size           = 5
+  min_size           = 3
+
+  mixed_instances_policy {
+    launch_template {
+      launch_template_specification {
+        launch_template_id = "${aws_launch_template.example.id}"
+      }
+
+    }
+  }
+}
