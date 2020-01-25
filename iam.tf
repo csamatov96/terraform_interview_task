4f260e16540f1157ce7b5589bde6d6cf06b199a2
csamatov96 <csamatov96@gmail.com>
2020-01-25 12:58:54 -0600

    policy added

--- iam.tf
+++ iam.tf
@@ -1,44 +1,18 @@
-resource "aws_iam_role" "role" {
-  name = "test-role"
-
-  assume_role_policy = <<EOF
-    {
-      "Version": "2012-10-17",
-      "Statement": [
-        {
-          "Action": "sts:AssumeRole",
-          "Principal": {
-            "Service": "ec2.amazonaws.com"
-          },
-          "Effect": "Allow",
-          "Sid": ""
-        }
-      ]
-    }
-EOF
-}
-
-resource "aws_iam_policy" "policy" {
-  name        = "test-policy"
-  description = "A test policy"
-
+resource “aws_iam_policy” “policy” { 
+  name = “test_policy3” 
+  path = “/“ 
+  description = “My test policy” 
   policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "ec2:Describe*"
-      ],
-      "Effect": "Allow",
-      "Resource": "*"
-    }
-  ]
-}
+{ 
+“Version”: “2012-10-17”, 
+“Statement”: [ 
+{ 
+“Sid”: “VisualEditor0”, 
+“Effect”: “Allow”, 
+“Action”: “ec2:*”, 
+“Resource”: “*” 
+} 
+] 
+} 
 EOF
-}
-
-resource "aws_iam_role_policy_attachment" "test-attach" {
-  role       = "${aws_iam_role.role.name}"
-  policy_arn = "${aws_iam_policy.policy.arn}"
 }
