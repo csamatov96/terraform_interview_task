995927958f12caea3f0532bae165cd74bfe48b22
csamatov96 <csamatov96@gmail.com>
2020-01-25 13:30:12 -0600

    changes

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-resource "aws_iam_role" "test" {
-  name               = "test-role"
+resource "aws_iam_role" "custom-role" {
+  name               = "custom-role"
   assume_role_policy = "${file("assume-role-policy.json")}"
 }
 
