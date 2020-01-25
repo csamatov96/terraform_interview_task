d1520fb3b44e560ffceeaa3f3fbfded4eb7577e3
csamatov96 <csamatov96@gmail.com>
2020-01-25 12:38:31 -0600

    changes

--- assume_policy.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_iam_role" "s3_access_role" {
-  name               = "s3-role"
-  assume_role_policy = "${file("policys3bucket.json")}"
-}
--- policy.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_iam_policy" "policy" {
-  name        = "test-policy"
-  description = "A test policy"
-  policy      = "${file("policys3bucket.json")}"
-}
