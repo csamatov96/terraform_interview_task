16f7976e4ca45947ae1b0bd085f96313696848aa
csamatov96 <csamatov96@gmail.com>
2020-01-25 12:59:51 -0600

    changes

--- iam.tf
+++ iam.tf
@@ -1,10 +1,10 @@
-resource “aws_iam_policy” “policy” { 
-  name = “test_policy3” 
-  path = “/“ 
-  description = “My test policy” 
+resource "aws_iam_policy" "policy" { 
+  name = "test_policy3" 
+  path = "/" 
+  description = "My test policy"
   policy = <<EOF
 { 
-“Version”: “2012-10-17”, 
+“Version”: "2012-10-17", 
 “Statement”: [ 
 { 
 “Sid”: “VisualEditor0”, 
