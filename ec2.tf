19bad54f0a872ecbee9c2955f767962f1f96fb36
csamatov96 <csamatov96@gmail.com>
2020-01-25 13:41:48 -0600

    changes

--- ec2.tf
+++ ec2.tf
@@ -15,7 +15,6 @@ output "ami" {
 resource "aws_instance" "web" {
     ami           = data.aws_ami.centos.id
     instance_type = "t2.micro"
-    key_name = aws_key_pair.terraform
     security_groups = ["allow_http_https"]
     user_data = file("userdata_file")
 
